171f5717cd9410fb7e4fa0ba2af29a0aea865e5f
Dennis Shulgin <dennisshulgin2017@yandex.ru>
2022-06-20 22:28:23 +0300

    prod

--- unit_test2.py
+++ unit_test2.py
@@ -8,7 +8,7 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-API_BASEURL = "http://localhost:80"
+API_BASEURL = "https://scroll-1803.usr.yandex-academy.ru"
 
 ROOT_ID = "069cb8d7-bbdd-47d3-ad8f-82ef4c269df1"
 
@@ -273,7 +273,7 @@ def test_all():
     test_nodes()
     #test_sales()
     #test_stats()
-    test_delete()
+    #test_delete()
 
 
 def main():
